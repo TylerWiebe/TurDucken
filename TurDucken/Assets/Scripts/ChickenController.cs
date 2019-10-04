17a61360ac4604593d77a2071c413fd160207195
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-04 10:53:39 -0700

    Added Chicken Movment and Basic Scripts
    Tyler

---
+++ TurDucken/Assets/Scripts/ChickenController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenController : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movement  * speed;
+    }
+}
