ef200a9df29ecf29db55922ac8299e7a14659d72
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-04 11:53:56 -0700

    Chicken Collision Testing

--- TurDucken/Assets/Scripts/ChickenController.cs
+++ TurDucken/Assets/Scripts/ChickenController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChickenController : MonoBehaviour
 {
 
-    public float speed = 0.5f;
+    public float speed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,7 @@ public class ChickenController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         transform.position += movement  * speed;
     }
