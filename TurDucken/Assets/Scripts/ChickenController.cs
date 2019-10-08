59fae3ed69da9e5a8e2d06255e061663d74cee58
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-07 17:07:36 -0700

    Finished Chicken Movement
    The movement of the chicken has been completed, The collision box can be controlled in the Chicken GameObject. The variables are Farm Height and Farm Width.  Under the same script we can control the speed at which the chicken moves

--- TurDucken/Assets/Scripts/ChickenController.cs
+++ TurDucken/Assets/Scripts/ChickenController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ChickenController : MonoBehaviour
 {
 
     public float speed = 0.1f;
+    public float FarmWidth = 2;
+    public float FarmHeight = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,42 @@ public class ChickenController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        transform.position += movement  * speed;
+
+        float DeltaX = Input.GetAxis("Horizontal") * speed;
+        float DeltaY = Input.GetAxis("Vertical") * speed;
+        float InitX = transform.position.x;
+        float InitY = transform.position.y;
+        float NextPosX = InitX + DeltaX;
+        float NextPosY = InitY + DeltaY;
+        if (Math.Abs(NextPosX) >= FarmWidth)
+        {
+            DeltaX = 0;
+            if (NextPosX > 0)
+            {
+                transform.position = new Vector3(FarmWidth, transform.position.y, 0);
+            }
+            else
+            {
+                transform.position = new Vector3(-FarmWidth, transform.position.y, 0);
+            }
+        }
+        if (Math.Abs(NextPosY) >= FarmHeight)
+        {
+            DeltaY = 0;
+            if (NextPosY > 0)
+            {
+                transform.position = new Vector3(transform.position.x, FarmHeight, 0);
+            }
+            else
+            { 
+                transform.position = new Vector3(transform.position.x, -FarmHeight, 0);
+            }
+        }
+
+        Vector3 movement = new Vector3(DeltaX, DeltaY, 0f);
+        transform.position += movement;
+
     }
 }
