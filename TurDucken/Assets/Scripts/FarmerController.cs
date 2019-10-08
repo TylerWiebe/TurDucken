350718f34fc2f4b8f27041c92b2a862f571e611e
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-07 17:55:47 -0700

    Added cursor follow mouse input

--- TurDucken/Assets/Scripts/FarmerController.cs
+++ TurDucken/Assets/Scripts/FarmerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FarmerController : MonoBehaviour
 {
+    public float BlastRadius = 0.25f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +12,21 @@ public class FarmerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 crosshairPos = new Vector3(mousePosition.x, mousePosition.y, 0);
+        transform.position = crosshairPos;
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject chicken = GameObject.Find("Chicken");
+            Vector3 chickenPos = chicken.transform.position;
+            float DistBetween = Mathf.Sqrt((Mathf.Pow(Mathf.Abs(chickenPos.x - crosshairPos.x),2) + Mathf.Pow(Mathf.Abs(chickenPos.y - crosshairPos.y),2)));
+            if (DistBetween <= BlastRadius)
+            {
+                System.Console.Write("This is a test");
+            }
+        }
     }
 }
