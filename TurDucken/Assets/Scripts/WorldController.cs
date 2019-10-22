c577675bbb658ef4d086638b10a70c15bd6b1875
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-21 18:24:56 -0700

    Finished Crosshair implementation
    Finished the Crosshair implementation and started the checkwin

--- TurDucken/Assets/Scripts/WorldController.cs
+++ TurDucken/Assets/Scripts/WorldController.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WorldController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timeLeft = 60f;
+    private bool timerStart = true;
+    public Text timerText;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (timerStart)
+        {
+            timeLeft -= Time.deltaTime;
+            timerText.text = timeLeft.ToString();
+        }
+
+        GameObject farmer = GameObject.Find("Farm");
+        FarmerController s = farmer.GetComponent<FarmerController>();
+        GameObject chicken = GameObject.Find("Chicken");
+        ChickenController cs = chicken.GetComponent<ChickenController>();
         
+        if (s.getChickenHit())
+        {
+
+        }
     }
 }
