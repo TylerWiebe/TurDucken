67e9a749e2e052d7f1c807cf536a6302b80af434
JacobDixon <buddypinkpaws@gmail.com>
2019-10-23 09:38:24 -0700

    Chicken Animation
    Chicken animations implemented- still trying to figure out how to make the chicken actually move...

---
+++ TurDucken/Assets/playermovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public float moveSpeed 5f;    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
