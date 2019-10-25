8cec5981244b53ba2066a9abc378c93e8d23ff15
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-25 10:10:46 -0700

    updated to 2019.2.10f1

--- TurDucken/Assets/Scripts/WorldController.cs
+++ TurDucken/Assets/Scripts/WorldController.cs
@@ -9,6 +9,10 @@ public class WorldController : MonoBehaviour
     private float timeLeft = 60f;
     private bool timerStart = true;
     public Text timerText;
+    GameObject farmer = GameObject.Find("Farm");
+    FarmerController s = farmer.GetComponent<FarmerController>();
+    GameObject chicken = GameObject.Find("Chicken");
+    ChickenController cs = chicken.GetComponent<ChickenController>();
 
     // Update is called once per frame
     void FixedUpdate()
@@ -19,14 +23,29 @@ public class WorldController : MonoBehaviour
             timerText.text = timeLeft.ToString();
         }
 
-        GameObject farmer = GameObject.Find("Farm");
-        FarmerController s = farmer.GetComponent<FarmerController>();
-        GameObject chicken = GameObject.Find("Chicken");
-        ChickenController cs = chicken.GetComponent<ChickenController>();
         
+        //Check win
         if (s.getChickenHit())
         {
 
         }
+    }
+
+    //resets the game
+    private void restartGame()
+    {
+
+    }
+    //one object that slides up into the cameral view
+    //change the sprite to match the winner
+    private void winBanners(bool ChickenWin)
+    {
+        
+    }
+
+    //an overlay
+    private void StartMenu()
+    {
+
     }
 }
