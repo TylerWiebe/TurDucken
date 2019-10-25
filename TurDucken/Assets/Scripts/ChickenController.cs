78b472eb49c9c67fd3a17e03b14097c725dcae65
Tyler Wiebe <tyler.d.wiebe@gmail.com>
2019-10-25 13:03:13 -0700

    Fixed Chicken Animation
    Chicken walking animation is completed
    Added crosshair sprite
    changed some file structure

--- TurDucken/Assets/Scripts/ChickenController.cs
+++ TurDucken/Assets/Scripts/ChickenController.cs
@@ -9,6 +9,7 @@ public class ChickenController : MonoBehaviour
     public float speed = 0.1f;
     public float FarmWidth = 2;
     public float FarmHeight = 2;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +22,22 @@ public class ChickenController : MonoBehaviour
 
         float DeltaX = Input.GetAxis("Horizontal") * speed;
         float DeltaY = Input.GetAxis("Vertical") * speed;
+        Debug.Log(Input.GetAxis("Horizontal"));
         float InitX = transform.position.x;
         float InitY = transform.position.y;
         float NextPosX = InitX + DeltaX;
         float NextPosY = InitY + DeltaY;
+        anim = GetComponent<Animator>();
+        if(DeltaY != 0 || DeltaX != 0)
+        {
+            anim.SetFloat("Speed", 0.1f);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0f);
+        }
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         if (Math.Abs(NextPosX) >= FarmWidth)
         {
             DeltaX = 0;
